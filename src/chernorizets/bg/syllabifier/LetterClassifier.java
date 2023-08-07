fcb9f6b2b45a1ea2db5a9edacd1f475c1538c015
Ruslan Dimov <rhydymov@gmail.com>
2023-08-06 21:31:41 -0700

    Letter classifier

---
+++ src/chernorizets/bg/syllabifier/LetterClassifier.java
@@ -0,0 +1,57 @@
+package chernorizets.bg.syllabifier;
+
+import java.util.Set;
+
+class LetterClassifier {
+    static final Set<Character> VOWELS = Set.of(
+            'а', 'ъ', 'о', 'у', 'е', 'и', 'ю', 'я'
+    );
+
+    static final Set<Character> SONORANTS = Set.of(
+            'л', 'м', 'н', 'р', 'й'
+    );
+
+    static final Set<Character> STOPS = Set.of(
+            'б', 'п', 'г', 'к', 'д', 'т'
+    );
+
+    static final Set<Character> FRICATIVES = Set.of(
+            'в', 'ф', 'ж', 'ш', 'з', 'с', 'х'
+    );
+
+    static final Set<Character> AFFRICATES = Set.of(
+            'ч', 'ц'
+    );
+
+    static boolean isVowel(char ch) {
+        return VOWELS.contains(ch);
+    }
+
+    static boolean isConsonant(char ch) {
+        return ch == 'щ'
+                || isSonorant(ch)
+                || isStop(ch)
+                || isFricative(ch)
+                || isAffricate(ch);
+    }
+
+    static boolean isPalatalizer(char ch) {
+        return ch == 'ь';
+    }
+
+    static boolean isSonorant(char ch) {
+        return SONORANTS.contains(ch);
+    }
+
+    static boolean isStop(char ch) {
+        return STOPS.contains(ch);
+    }
+
+    static boolean isFricative(char ch) {
+        return FRICATIVES.contains(ch);
+    }
+
+    static  boolean isAffricate(char ch) {
+        return AFFRICATES.contains(ch);
+    }
+}
